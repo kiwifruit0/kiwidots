2db87cfb43ecb2f4ae51e6c5dbb2441e9d9a2f50
kiwi <79637319+kiwifruit0@users.noreply.github.com>
2025-12-14 19:53:30 +0000

    fixed nvim tmux navigation

--- config/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ config/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,19 +1,18 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	lazy = false,
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-		"TmuxNavigatorProcessList",
-	},
-	keys = {
-		{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>", silent = true },
-		{ "<C-j>", "<cmd>TmuxNavigateDown<cr>", silent = true },
-		{ "<C-k>", "<cmd>TmuxNavigateUp<cr>", silent = true },
-		{ "<C-l>", "<cmd>TmuxNavigateRight<cr>", silent = true },
-		{ "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", silent = true },
-	},
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
 }
